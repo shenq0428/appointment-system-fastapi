05a517943fd3e5c45763defb76d418f6c00aaa23
shenq0428 <wzshenq0428@gmail.com>
2026-01-28 11:04:29 +0800

    feat: implement appointment api and connect to main app

--- app/api/endpoints/appointments.py
+++ app/api/endpoints/appointments.py
@@ -1,1 +1,17 @@
+from fastapi import APIRouter
+# 从我们之前写的 schemas 里导入模板
+from app.schemas.appointment import AppointmentCreate
 
+router = APIRouter()
+
+# 模拟一个临时数据库（程序重启后数据会清空，后续我们会学如何连真数据库）
+fake_appointment_db = []
+
+@router.post("/")
+def create_appointment(item: AppointmentCreate):
+    fake_appointment_db.append(item)
+    return {"message": "make appointment successfully","data": item}
+
+@router.get("/")
+def get_all_appointment():
+    return fake_appointment_db
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from app.api.endpoints import appointments
+
 app = FastAPI()
 
+# 将 appointments 路由挂载到主程序，并加上前缀
+app.include_router(appointments.router, prefix="/appointments",tags=["预约管理"])
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
